3a3bc030d591917aeb629a493b934bb593ff6bca
Emiliano Menéndez <emystein@gmail.com>
2023-03-24 22:13:40 -0300

    Clean GridPrinter

--- src/test/kotlin/ar/com/flow/kata/aligncolumns/GridPrinter.kt
+++ src/test/kotlin/ar/com/flow/kata/aligncolumns/GridPrinter.kt
@@ -2,14 +2,14 @@ package ar.com.flow.kata.aligncolumns
 
 class GridPrinter {
     fun print(lines: List<List<String>>): String {
-        val borderRowColumnSeparators =
-            Columns.of(lines).fold("") { accumulator, column -> accumulator + ("-".repeat(column.width) + "*") }
-            .removeSuffix("*")
-        val borderRow = "*$borderRowColumnSeparators*\n"
-        return borderRow + lines.fold(""){ accumulator, line -> accumulator + wrapLine(line)} + borderRow
+        val borderRow =
+            Columns.of(lines).map { column -> "-".repeat(column.width) }
+                             .joinToString( separator = "*", prefix = "*", postfix = "*")
+        val wrappedLines = lines.joinToString(separator = "") { line -> wrapLine(line) }
+        return borderRow + "\n" + wrappedLines + borderRow + "\n"
     }
 
     private fun wrapLine(words: List<String>): String {
-        return words.joinToString(separator = "|", prefix = "|", postfix = "|") + "\n"
+        return words.joinToString(separator = "|", prefix = "|", postfix = "|\n")
     }
 }
