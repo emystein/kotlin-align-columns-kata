1e74b4e1e380dea9a71447dcc58bdd9f5219185b
Emiliano Menéndez <emystein@gmail.com>
2023-03-26 10:23:47 -0300

    Clean Table.

--- src/main/kotlin/ar/com/flow/kata/aligncolumns/Table.kt
+++ src/main/kotlin/ar/com/flow/kata/aligncolumns/Table.kt
@@ -7,17 +7,12 @@ data class Table(val lines: List<Line>, val columns: List<Column>) {
 
 	fun alignColumns(alignment: Alignment): Table {
 		val alignedLines = this.lines.map { line -> line.alignColumns(alignment) }
-		return Table(alignedLines, columns)
+		return Table(alignedLines, this.columns)
 	}
 
 	companion object {
 		fun from(input: List<List<String>>): Table {
 			val lines = Lines.from(input)
-			val columns = Columns.ofLines(lines)
-			return Table(lines, columns)
-		}
-
-		fun fromLines(lines: List<Line>): Table {
 			val columns = Columns.ofLines(lines)
 			return Table(lines, columns)
 		}
